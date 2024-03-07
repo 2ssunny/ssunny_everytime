f7ceeb1a5f4739eecd3d2fd834f38667c14252b2
Juho Lee <eungseoni@gmail.com>
2024-03-06 21:32:38 -0500

    feat: make main page

--- App/App.js
+++ App/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from'react-native';
+import { Link } from 'expo-router';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome. Login to use all features.</Text>
+            <Link href="app/index.js">Home</Link>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    }
+})
